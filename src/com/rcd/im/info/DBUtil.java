a14867c80497a216915343d140a2a4f0181c5144
getan <getanlinux@gmail.com>
2014-04-17 16:33:42 +0800

    修改数据库密码

--- src/com/rcd/im/info/DBUtil.java
+++ src/com/rcd/im/info/DBUtil.java
@@ -2,13 +2,7 @@ package com.rcd.im.info;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 public class DBUtil {
 
@@ -16,7 +10,7 @@ public class DBUtil {
 		String driverClassName = "com.mysql.jdbc.Driver";
 		String url = "jdbc:mysql://localhost/movie";
 		String username = "root";
-		String password = "HUHUI";
+		String password = "f";
 		Connection conn = null;
 		try {
 			Class.forName(driverClassName);
